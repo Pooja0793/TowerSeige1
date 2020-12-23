9d644f2260b91dc86ebb6de903542096a12401a8
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2020-12-23 10:07:41 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Block {
+  constructor(x,y,width,height) {
+    var options = {
+        isStatic: false
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    push();
+    translate(pos.x,pos.y);
+    rotate(this.body.angle);
+    
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
